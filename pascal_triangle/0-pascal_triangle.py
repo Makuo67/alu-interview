420e5acc4e5612a8d45c7e7146cc0494ffb6beef
Makuo67 <okekemakuo67@gmail.com>
2023-03-18 01:31:33 +0200

    :robot: automated commit of one or many files

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -5,6 +5,7 @@ A function that return pascal triangle
 
 
 def pascal_triangle(n):
+    """returns a list of lists of integers representing the Pascal’s triangle of n"""
     if n <= 0:
         return []
     triangle = [[1]]
